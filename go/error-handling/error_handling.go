daad9d6285206917bd04172a908edf1efaca3377
bxhzzx <bxhzzx>
2020-12-02 16:04:26 +0800

    complete error-handling.

--- go/error-handling/error_handling.go
+++ go/error-handling/error_handling.go
@@ -1,1 +1,33 @@
 package erratum
+
+func Use(opener func() (Resource, error), s string) (err error) {
+	resource, err := opener()
+	for {
+		if err != nil {
+			if _, ok := err.(TransientError); !ok {
+				return err
+			} else {
+				resource, err = opener()
+				continue
+			}
+		} else {
+			break
+		}
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			var ok bool
+			if _, ok = r.(FrobError); !ok {
+				err = r.(error)
+			} else {
+				resource.Defrob(r.(FrobError).defrobTag)
+				err = r.(FrobError)
+			}
+		}
+		resource.Close()
+	}()
+
+	resource.Frob(s)
+	return
+}
